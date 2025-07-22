49dbbcd39e2130e1d19c231446159d78777adf42
jeevesh56 <jeevesh2910@gmail.com>
2025-07-22 18:51:04 +0530

    error fixed

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -4,22 +4,17 @@ int main() {
     double num1, num2;
     char op;
 
-    // Display a welcome message
     std::cout << "Simple Calculator\n";
 
-    // Get first number
     std::cout << "Enter first number: ";
     std::cin >> num1;
 
-    // Get operator
     std::cout << "Enter an operator (+, -, *, /): ";
     std::cin >> op;
 
-    // Get second number
     std::cout << "Enter second number: ";
     std::cin >> num2;
 
-    // Perform calculation based on operator
     if (op == '+') {
         std::cout << "Result: " << (num1 + num2) << std::endl;
     } else if (op == '-') {
